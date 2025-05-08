5f35ed9137b3de5edca76a0d4cae220493c8475a
Ali Habib <habibali@outlook.com>
2025-05-08 19:37:25 +0400

    video add

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,11 +43,16 @@ const Hero = () => {
           </div>
           <div className="lg:col-span-2 relative animate-scale-in">
             <div className="aspect-[4/3] bg-card/40 rounded-2xl glass-card overflow-hidden">
-              <img
+           {/* <img
                 src="https://images.unsplash.com/photo-1540575467063-178a50c2df87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
                 alt="UAE Events"
                 className="w-full h-full object-cover opacity-80"
-              />
+              />*/}
+              <video autoplay loop muted playsinline>
+              <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+              </video>
+              
               <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/40 to-transparent flex items-end">
                 <div className="p-6 text-white">
                   <p className="text-lg font-semibold">Dubai International Conference</p>
